8fe26dfb910018063668673829cfabc93266b214
Amar Raja <amaryraja@gmail.com>
2019-12-03 11:50:50 +0700

    Pass noun and verb as parameters - handy for part 2

--- day02/part1.exs
+++ day02/part1.exs
@@ -6,8 +6,15 @@ defmodule Day02 do
     |> run()
   end
 
-  def run(input) when is_list(input) do
-    interpret(input, 0)
+  def run(input, noun, verb) do
+    [a, b, c | tl] =
+      input
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    commands = [a, noun, verb] ++ tl
+
+    interpret(commands, 0)
   end
 
   def interpret(acc) do
@@ -55,13 +62,6 @@ defmodule Test do
   end
 
   test "part1" do
-    [a, b, c | tl] =
-      File.read!("input1.txt")
-      |> String.split(",")
-      |> Enum.map(&String.to_integer/1)
-
-    commands = [a, 12, 2] ++ tl
-
-    IO.inspect(Day02.run(commands), label: "part1")
+    IO.inspect(Day02.run(File.read!("input1.txt"), 12, 2), label: "part1")
   end
 end
