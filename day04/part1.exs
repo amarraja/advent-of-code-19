67f32e4fc22b5dfc1b0c896100874bea2675cfd4
Amar Raja <amaryraja@gmail.com>
2019-12-05 08:33:11 +1100

    Part 2

--- day04/part1.exs
+++ day04/part1.exs
@@ -14,6 +14,34 @@ defmodule Day04 do
   def validate([_ | tl], r), do: validate(tl, r)
 end
 
+defmodule Day04Part2 do
+  def run(input) do
+    [upper, lower] = input |> String.split("-") |> Enum.map(&String.to_integer/1)
+
+    upper..lower
+    |> Enum.filter(&validate/1)
+    |> Enum.count()
+  end
+
+  def validate(int), do: validate(Integer.digits(int), %{all_increase: true, has_double: false})
+  def validate([], r), do: r.all_increase && r.has_double
+
+  def validate([h, h | _] = l, r) do
+    case count_sequence(l) do
+      {2, rest} -> validate(rest, %{r | has_double: true})
+      {_, rest} -> validate(rest, r)
+    end
+  end
+
+  def validate([a, b | _] = l, r) when a > b, do: validate(tl(l), %{r | all_increase: false})
+  def validate([_ | tl], r), do: validate(tl, r)
+
+  def count_sequence([h | t] = l), do: count_sequence(l, h, 0)
+  def count_sequence([] = t, value, acc), do: {acc, [value]}
+  def count_sequence([h | t], value, acc) when h == value, do: count_sequence(t, value, acc + 1)
+  def count_sequence(l, value, acc), do: {acc, [value | l]}
+end
+
 ExUnit.start()
 
 defmodule Test do
@@ -30,5 +58,17 @@ defmodule Test do
     "197487-673251"
     |> Day04.run()
     |> IO.inspect(label: "part1")
+  end
+
+  test "validate number part 2" do
+    assert Day04Part2.validate(112_233) == true
+    assert Day04Part2.validate(123_444) == false
+    assert Day04Part2.validate(111_122) == true
+  end
+
+  test "part2" do
+    "197487-673251"
+    |> Day04Part2.run()
+    |> IO.inspect(label: "part2")
   end
 end
