87e0685439dca986fc728bc92a479586a98028af
Amar Raja <amaryraja@gmail.com>
2019-12-11 16:08:20 +1100

    Part2

--- day08/part1.exs
+++ day08/part1.exs
@@ -9,14 +9,38 @@ defmodule Day08 do
     |> Enum.min_by(fn %{"0" => zeroes} -> length(zeroes) end)
     |> (fn %{"1" => ones, "2" => twos} -> length(ones) * length(twos) end).()
   end
+
+  def run2(input) do
+    input
+    |> String.codepoints()
+    |> Enum.chunk_every(25 * 6)
+    |> Enum.reduce(&stack/2)
+    |> Enum.map(fn
+      "0" -> " "
+      "1" -> "*"
+    end)
+    |> Enum.chunk_every(25)
+    |> Enum.join("\n")
+  end
+
+  def stack([], []), do: []
+  def stack([h | tl1], ["2" | tl2]), do: [h | stack(tl1, tl2)]
+  def stack([_ | tl1], [h | tl2]), do: [h | stack(tl1, tl2)]
 end
 
 ExUnit.start()
 
 defmodule Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
   test "part1" do
     IO.inspect(Day08.run(File.read!("input.txt")), label: "part1")
+  end
+
+  test "part2" do
+    output = Day08.run2(File.read!("input.txt"))
+    IO.puts("-----------\n\n")
+    IO.puts(output)
+    IO.puts("\n\n-----------")
   end
 end
